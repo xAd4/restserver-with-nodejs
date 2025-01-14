f4603bec4f6a9e58fe6f95368e8be6075498c976
Angel <gabrielcardozo3579@gmail.com>
2025-01-14 17:55:22 -0400

    Add user.email validation middleware

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,35 @@
+const { validationResult, check } = require("express-validator");
 const User = require("../models/User");
-const bcryptjs = require("bcryptjs");
 
-const createUser = async function (req, res) {
+// Middleware de validación
+const validateUser = [
+  check("email").isEmail().withMessage("Must be valid email."),
+];
+
+const createUser = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
   try {
     const { name, email, password, img, role } = req.body;
     const user = new User({ name, email, password, img, role });
 
+    const emailExist = await User.findOne({ email });
+
+    if (emailExist) {
+      return res.status(400).json({ message: "Email exists already" });
+    }
+
     await user.save();
     res.status(201).json(user);
   } catch (error) {
-    res.status(500).json({ message: `Error: ${error} ` });
+    res.status(400).json({ message: `Error: ${error} ` });
   }
 };
 
-module.exports = createUser;
+module.exports = {
+  validateUser,
+  createUser,
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const createUser = require("../controllers/user.controller");
+const { createUser, validateUser } = require("../controllers/user.controller");
 
 const router = express.Router();
-router.post("/", createUser);
+router.post("/", validateUser, createUser);
 
 module.exports = router;
