3f1299c4e41a3cb9f0f25d9155fc1ddcf4f86893
Angel <gabrielcardozo3579@gmail.com>
2025-01-14 17:37:35 -0400

    Add password encryption

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,12 +1,11 @@
 const User = require("../models/User");
+const bcryptjs = require("bcryptjs");
 
 const createUser = async function (req, res) {
   try {
-    // Creación de nueva instancia
     const { name, email, password, img, role } = req.body;
     const user = new User({ name, email, password, img, role });
 
-    // Guardar la instancia creada
     await user.save();
     res.status(201).json(user);
   } catch (error) {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -9,5 +10,17 @@ const UserSchema = new mongoose.Schema({
   state: { type: Boolean, default: true },
   google: { type: Boolean, default: false },
 });
+
+// Encriptar password
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next;
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bycrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
