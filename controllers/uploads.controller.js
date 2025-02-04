91ac7ae32f12aed6304057d68b44f921288ddc0e
Angel <gabrielcardozo3579@gmail.com>
2025-02-04 16:31:26 -0400

    Update file upload handling to use UUID for unique filenames; add uuid package

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const uploads = (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archive) {
@@ -18,7 +19,9 @@ const uploads = (req, res = response) => {
       .status(400)
       .json({ msg: `The archive extension must be ${extensionAllowed}` });
 
-  const uploadPath = path.join(__dirname, "../uploads", archive.name);
+  // Name uuid
+  const nameTemp = uuidv4() + "." + extension;
+  const uploadPath = path.join(__dirname, "../uploads", nameTemp);
   archive.mv(uploadPath, (err) => {
     if (err) return res.status(500).json({ err });
     res.json({ msg: "File uploaded to" + uploadPath });
